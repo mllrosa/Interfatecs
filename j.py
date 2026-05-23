a8cc3e26eefb6d3d2ec4acb03f7138af572cd035
ROSA <mllr8sa@gmail.com>
2026-05-23 15:49:47 +0000

    codigo j

---
+++ j.py
@@ -0,0 +1,16 @@
+entrada1 = int(input())
+pim = 4
+contpim = 1
+
+for i in range(entrada1 + 1):
+    
+    if i == 0:
+        pass
+
+    elif i % 4 != 0:
+        print(i, end=' ')
+
+    else:
+        print('pim ', end='')
+    
+    
