f02aaa1ede51017250477e7d5fabf6614ba1e4ff
ROSA <mllr8sa@gmail.com>
2026-05-23 18:32:32 +0000

    Conseguimos finalmente

--- j.py
+++ j.py
@@ -1,16 +1,18 @@
-entrada1 = int(input())
-pim = 4
-contpim = 1
+hora_total, brinquedos_total = map(int, input().split())
+tempo_total = 60*hora_total
+tempo_gasto = 0
+nota_brinquedo = 0
 
-for i in range(entrada1 + 1):
-    
-    if i == 0:
-        pass
+for i in range(brinquedos_total):
+    nomebrinquedo1 = input()
+    nota, tempo_brincar = map(int, input().split())
 
-    elif i % 4 != 0:
-        print(i, end=' ')
+    tempo_gasto = tempo_brincar + tempo_gasto
+    nota_brinquedo = nota_brinquedo + nota
 
-    else:
-        print('pim ', end='')
-    
-    
+if tempo_gasto > tempo_total:
+    print(0)
+else:
+    print(nota_brinquedo)
+
+
